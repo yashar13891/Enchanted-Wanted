a9ef8bff7f05c14c4891ab8da6ba6751f1c7fdfb
USER <vsdrrt124@gmail.com>
2025-02-07 22:45:29 +0330

    Add Config & Clean The Main

--- src/main/java/org/yashar/enchantedWanted/EnchantedWanted.java
+++ src/main/java/org/yashar/enchantedWanted/EnchantedWanted.java
@@ -1,41 +1,36 @@
 package org.yashar.enchantedWanted;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.permissions.Permission;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import org.yashar.enchantedWanted.managers.ConfigManager;
-
-import static org.yashar.enchantedWanted.managers.ConfigManager.setupConfig;
-
 import org.yashar.enchantedWanted.storages.*;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
 public final class EnchantedWanted extends JavaPlugin {
-    public static Plugin instance;
-    private static Logger logger;
-    private DatabaseManager database;
 
+    private DatabaseManager database;
+    private static Logger logger;
+    private static EnchantedWanted plugin;
+
+    public static Logger getPluginLogger() {
+        return logger;
+    }
+
+    public static EnchantedWanted getInstance() {
+        return plugin;
+    }
 
     @Override
     public void onEnable() {
-        instance = this;
 
-        setupConfig();
-
-        this.registerEvents();
-        this.registerCommands();
-
+        plugin = this;
         logger = getLogger();
-        logger.info("Enchanted Wanted Enabled! Thanks For Using (:");
 
+        //Database SetUp
         String databaseType = ConfigManager.getConfig().getString("database.type", "sqlite");
         if (databaseType.equalsIgnoreCase("mysql")) {
             database = new MySQLManager();
@@ -51,17 +46,14 @@ public final class EnchantedWanted extends JavaPlugin {
             getLogger().severe("[Database] Failed to connect to database!");
             getServer().getPluginManager().disablePlugin(this);
         }
+
+        logger.info("Enchanted Wanted Enabled! Thanks For Using (:");
     }
 
     @Override
     public void onDisable() {
-        saveConfig();
         database.disconnect();
         getLogger().severe("[Database] DataBase Disconnected!");
-    }
-
-    public static Logger getPluginLogger() {
-        return logger;
     }
 
     List<PluginCommand> commands = new ArrayList<>();
@@ -73,17 +65,5 @@ public final class EnchantedWanted extends JavaPlugin {
             command.setPermission(permission.toString());
             commands.add(command);
         }
-    }
-
-    public static JavaPlugin getInstance() {
-        return (JavaPlugin) instance;
-    }
-
-    public void registerCommands() {
-
-    }
-
-    public void registerEvents() {
-        PluginManager pluginManager = Bukkit.getPluginManager();
     }
 }
