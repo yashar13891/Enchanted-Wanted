bd75f71a4b9779c5b70ae1d3c5e0925d7c81cc4c
Yashar <yashar13891@gmail.com>
2025-02-07 06:02:50 -0800

    Add registercommand method

--- src/main/java/org/yashar/enchantedWanted/EnchantedWanted.java
+++ src/main/java/org/yashar/enchantedWanted/EnchantedWanted.java
@@ -1,6 +1,12 @@
 package org.yashar.enchantedWanted;
 
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class EnchantedWanted extends JavaPlugin {
 
@@ -14,4 +20,14 @@ public final class EnchantedWanted extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+    List<PluginCommand> commands = new ArrayList<>();
+    public void registerCommand(String name, CommandExecutor executor, Permission permission) {
+        PluginCommand command = getCommand(name);
+        if (command != null) {
+            command.setExecutor(executor);
+            command.setPermission(permission.toString());
+            commands.add(command);
+        }
+    }
+
 }
