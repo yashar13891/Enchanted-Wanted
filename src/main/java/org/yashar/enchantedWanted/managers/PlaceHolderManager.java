072d3ebfdf10251b3ff62105ad1ca7708498e107
Yashar <yashar13891@gmail.com>
2025-02-07 10:49:22 -0800

    Edit PlaceHolderManager.java

--- src/main/java/org/yashar/enchantedWanted/managers/PlaceHolderManager.java
+++ src/main/java/org/yashar/enchantedWanted/managers/PlaceHolderManager.java
@@ -11,14 +11,13 @@ public class PlaceHolderManager extends PlaceholderExpansion {
 
     private final EnchantedWanted plugin;
     private DatabaseManager database;
-    private int getplayerwanted;
     public PlaceHolderManager(EnchantedWanted plugin) {
         this.plugin = plugin;
     }
 
     @Override
     public @NotNull String getIdentifier() {
-        return "enchantedwanted";
+        return "ew";
     }
 
     @Override
@@ -28,16 +27,24 @@ public class PlaceHolderManager extends PlaceholderExpansion {
 
     @Override
     public @NotNull String getVersion() {
-        return "2.0";
+        return "1.0";
     }
 
     @Override
     public String onPlaceholderRequest(Player player, String identifier) {
         switch (identifier) {
-            case "%enchantedwanted_profile_wanted%":
+            case "%ew_wanted%":
                 return String.valueOf(database.getWanted(player.getUniqueId()));
-            case "%enchantedwanted_profile_wanted_formatted%":
-                return
+            case "%ew_formatted_wanted%":
+                if (player == null) return "";
+
+                int wantedLevel = database.getWanted(player.getUniqueId());
+                wantedLevel = Math.max(wantedLevel, 0);
+                StringBuilder stars = new StringBuilder();
+                for (int i = 0; i < wantedLevel; i++) {
+                    stars.append("★");
+                }
+                return stars.toString();
         }
 
         return null;
