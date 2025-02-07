2f64c82667c427b6b5e055783bb8e676b8d4be30
Yashar <yashar13891@gmail.com>
2025-02-07 06:52:59 -0800

    Edit EnchantedWanted.java

--- src/main/java/org/yashar/enchantedWanted/EnchantedWanted.java
+++ src/main/java/org/yashar/enchantedWanted/EnchantedWanted.java
@@ -3,23 +3,23 @@ package org.yashar.enchantedWanted;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public final class EnchantedWanted extends JavaPlugin {
+    public static Plugin instance;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+    public void onDisable() {}
     List<PluginCommand> commands = new ArrayList<>();
     public void registerCommand(String name, CommandExecutor executor, Permission permission) {
         PluginCommand command = getCommand(name);
@@ -29,5 +29,13 @@ public final class EnchantedWanted extends JavaPlugin {
             commands.add(command);
         }
     }
+    public static JavaPlugin getInstance() {
+        return (JavaPlugin)instance;
+    }
+    public void registerCommands() {
 
+    }
+    public void registerEvents() {
+
+    }
 }
