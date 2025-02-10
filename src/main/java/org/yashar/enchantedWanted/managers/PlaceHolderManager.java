16256a9a2a8decf7440500082f04574c971e6f65
Misagh1 <misaghalivi@gmail.com>
2025-02-10 12:58:10 +0330

    Edited PlaceHolderManager.java / Edited config.yml

--- src/main/java/org/yashar/enchantedWanted/managers/PlaceHolderManager.java
+++ src/main/java/org/yashar/enchantedWanted/managers/PlaceHolderManager.java
@@ -2,17 +2,22 @@ package org.yashar.enchantedWanted.managers;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.yashar.enchantedWanted.EnchantedWanted;
 import org.yashar.enchantedWanted.storages.DatabaseManager;
+import org.yashar.enchantedWanted.utils.MessageUtils;
 
 public class PlaceHolderManager extends PlaceholderExpansion {
 
     private final DatabaseManager database;
+    private final FileConfiguration config;
 
-    public PlaceHolderManager(@NotNull DatabaseManager database) {
+    public PlaceHolderManager(@NotNull DatabaseManager database, @NotNull EnchantedWanted plugin) {
         this.database = database;
+        this.config = plugin.getConfig();
     }
 
     @Override
@@ -47,12 +52,19 @@ public class PlaceHolderManager extends PlaceholderExpansion {
         Bukkit.getLogger().info("Placeholder Requested: " + identifier + " for player: " + player.getName());
 
         int wantedLevel = Math.max(database.getWanted(player.getUniqueId()), 0);
-
         Bukkit.getLogger().info("Wanted Level for " + player.getName() + ": " + wantedLevel);
 
+        String filledStar = config.getString("wanted.filled", "&c★");
+        String emptyStar = config.getString("wanted.empty", "&7✩");
+        String wantedColor = config.getString("wanted.number", "&e");
+
+        String formattedWanted = MessageUtils.convertLegacyCodesToMiniMessage(filledStar).repeat(wantedLevel)
+                + MessageUtils.convertLegacyCodesToMiniMessage(emptyStar).repeat(6 - wantedLevel);
+        String wantedNumber = MessageUtils.convertLegacyCodesToMiniMessage(wantedColor) + wantedLevel;
+
         return switch (identifier) {
-            case "wanted" -> String.valueOf(wantedLevel);
-            case "formatted_wanted" -> "★".repeat(wantedLevel) + "✩".repeat(6 - wantedLevel);
+            case "wanted" -> wantedNumber;
+            case "formatted_wanted" -> formattedWanted;
             default -> null;
         };
     }
