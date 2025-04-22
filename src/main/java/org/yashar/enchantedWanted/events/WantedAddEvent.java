ca5ebadfb17b0656e49ad0231cceeaa08515bcd2
Riven <sagdast203@Gmail.com>
2025-04-22 18:53:27 +0330

    Update WantedAddEvent.java
    simplify WantedAddEvent with Lombok and implement Cancellable

--- src/main/java/org/yashar/enchantedWanted/events/WantedAddEvent.java
+++ src/main/java/org/yashar/enchantedWanted/events/WantedAddEvent.java
@@ -1,41 +1,31 @@
 package org.yashar.enchantedWanted.events;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
-public class WantedAddEvent extends Event {
+@RequiredArgsConstructor
+@Getter
+public class WantedAddEvent extends Event implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
+
+    @Setter
     private boolean cancelled;
+
     private final UUID uuid;
-    public WantedAddEvent(@NotNull UUID uuid) {
-        this.uuid = uuid;
-    }
 
-
-    public void setCancelled(boolean cancelled) {
-        this.cancelled = cancelled;
-    }
 
     public Player getPlayer() {
         Player p = Bukkit.getPlayer(uuid);
-        if (p == null) {
-            return null;
-        }
-        if (p.isOnline()) {
-            return p;
-        }
-        return null;
+        return (p != null && p.isOnline()) ? p : null;
     }
-
-    public boolean isCancelled() {
-        return cancelled;
-    }
-
 
     @Override
     public @NotNull HandlerList getHandlers() {
